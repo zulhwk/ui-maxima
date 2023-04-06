76d41b5cb297793cb4fdbe1901bdbfcaf3ec3aa0
telkom.yanuardi <yanuardi48@gmail.com>
2023-04-06 10:12:11 +0700

    footer design

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -4,6 +4,8 @@ export interface FooterProps extends ComponentProps<"footer"> {};
 
 export const Footer: FC<FooterProps> = ({ ...props }) => {
   return (
-    <footer>Footer</footer>
+    <footer className="bg-[#FFFF00] py-[35px] text-center font-normal text-[20px] leading-[30px] tracking-[-0.015em] text-[#00000080]/50">
+      © Maxima Post Production Studio
+    </footer>
   )
 };
--- src/components/layouts/index.tsx
+++ src/components/layouts/index.tsx
@@ -1,11 +1,11 @@
-import { ComponentProps, FC } from "react";
+import { ComponentProps } from "react";
 import { Navbar } from "./Navbar";
 import { Footer } from "./Footer";
 import { Outlet } from "react-router-dom";
 
 export interface LayoutProps extends ComponentProps<"div"> {};
 
-export const Layouts  = ({ ...props }) => {
+export const Layouts = ({ ...props }) => {
   return (
     <div>
       <Navbar />
